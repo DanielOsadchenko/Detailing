c9ea5560af2e07383aad9fd5ab3505502aea73cd
DanielOsadchenko <dm.osadchenko@gmail.com>
2022-11-27 12:46:21 +0100

    50%

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
+import React from 'react'
+import logo from '../images/fullLogo.png';
+import smallLogo from '../images/logo.png';
+import { Container, Hero, Header, Nav, Link, Logo, HeroBg, Title, HeroBtn, About, AboutTitle, LineStyle, AboutText, Card, CardTitle, CardBox, CardSet, CardText } from './App.styled';
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+
+    return <div>
+      <HeroBg>
+        <Header>
+          <Nav>
+            <Link href="/">Послуги</Link>
+            <Link href="/">Результат</Link>
+            <Link href="/">Контакти</Link>
+          </Nav>
+          <Link href="/"><img src={logo} alt="Detailing by Semenov" /></Link>
+        </Header>
+        <Hero>
+          <Container>
+            <Title>Студія детейлінгу</Title>
+            <HeroBtn type='button'>Обрати послугу</HeroBtn>
+          </Container>
+        </Hero>
+      </HeroBg>
+    
+      <About>
+        <LineStyle><AboutTitle>Про нас</AboutTitle></LineStyle>
+        <AboutText>
+          Дозвольте представитися, мене звати Микита Семенов. Detailing - (англ.) виконання максимально ретельного і делікатного очищення кузова і салону, полірування кузова автомобіля, нанесення захисних складів на всі його ділянки для надання Вашому автомобілю шоу-якості. У Детейлінг студії "Semenov" всі роботи я виконую сам. У мене немає секретарів, менеджерів і найманого персоналу. Причина цього проста. Я перфекціоніст, і я не можу довірити будь-кому догляд за Вашим автомобілем, з таким досвідом і ентузіазмом. Таким чином, Ви можете бути впевнені в персональному обслуговуванні від Semenov, і неперевершеному рівні робіт. Жодна деталь Вашого автомобіля не залишиться без уваги.
+          Наша детейлінг майстерня виконує повний комплекс по догляду за екстер'єром та інтер'єром авто.
+        </AboutText>
+        <Logo src={smallLogo} alt="Detailing by Semenov" />
+      </About>
+
+      <About>
+        <LineStyle><AboutTitle>Послуги</AboutTitle></LineStyle>
+      
+        <CardSet>
+          <Card>
+            <CardTitle>Полірування фар</CardTitle>
+            <CardBox>
+              <CardText>Полірування фар застосовується у випадках помутніння, пожовтіння або для усунення дефектів і подряпин, отриманих в результаті дрібних ДТП і пригод.</CardText>
+              <CardText>150zl</CardText>
+            </CardBox>
+          </Card>
+
+          <Card>
+            <CardTitle>Полірування кузову</CardTitle>
+            <CardBox>
+              <CardText>Процедура полірування допоможе повернути поверхні автомобіля дзеркальний блиск і позбутися дрібних дефектів.</CardText>
+              <CardText>від 200zl</CardText>
+            </CardBox>
+          </Card>
+
+          <Card>
+            <CardTitle>Хімчистка комплекс</CardTitle>
+            <CardBox>
+              <CardText>Комплекс хімчистки автомобіля зачіпає не тільки килимове покриття, сидіння, а й стелю, двері, приладову панель, пластикові деталі.</CardText>
+              <CardText>від 300zl</CardText>
+            </CardBox>
+          </Card>
+
+          <Card>
+            <CardTitle>Хімчистка елементу</CardTitle>
+            <CardBox>
+              <CardText>Хімчистка будь-якого елементу салону. Якщо ви розлили каву, або перевозили в багажнику брудні речі, вам може знадобитися ця послуга.</CardText>
+              <CardText>від 80zl</CardText>
+            </CardBox>
+          </Card>
+        </CardSet>
+      </About>
+
+      <About>
+        <LineStyle><AboutTitle>Результат</AboutTitle></LineStyle>
+
+        
+
+        <button type="button" class="previous">Previous</button>
+        <button type="button" class="next">Next</button>
+
+      </About>
     </div>
-  );
-};
+  
+  };
+
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,225 @@
+import styled from '@emotion/styled';
+import bgImage from '../images/background.jpg';
+
+
+export const Container = styled.div`
+width: 1440px;
+text-align: center;
+margin-top: 110px;`;
+
+export const Hero = styled.section``;
+
+export const Header = styled.header`
+display: flex;
+justify-content: space-between;
+padding-top: 40px;
+padding-left: 80px;
+padding-right: 80px;`;
+
+export const Nav = styled.nav`
+position: relative;
+::after {
+  content: '';
+  display: block;
+  border-bottom: 4px solid #E89636;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+`;
+
+export const HeroBg = styled.div`
+min-width: 1440px;
+
+// height: 800px;
+background-image: linear-gradient(rgba(47, 48, 58, 0.2), rgba(47, 48, 58, 0.2)), url(${bgImage});
+background-size: cover;
+background-position: center;
+background-repeat: no-repeat;
+margin-left: auto;
+margin-right: auto;
+`;
+
+export const Link = styled.a`
+font-style: normal;
+font-weight: 500;
+font-size: 36px;
+color: #E4D8C9;
+:hover {
+  color: #E89636
+}
+
+&+& {
+  margin-left:24px;
+}`;
+
+export const Logo = styled.img`
+margin-left: auto;
+margin-right: auto;
+margin-bottom: 40px;`;
+
+export const Title = styled.h1`
+color: #E4D8C9;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+font-weight: 700;
+font-size: 48px;
+letter-spacing: 0.4em;
+margin-bottom: 334px;
+`;
+
+export const HeroBtn = styled.button`
+
+margin-bottom: 40px;
+width: 500px;
+height:110px;
+border: 3px solid #E89636;
+filter: drop-shadow(2px 2px 50px rgba(148, 140, 160, 0.3));
+border-radius: 16px;
+font-weight: 700;
+font-size: 40px;
+line-height: 47px;
+color: #E89636;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+background: transparent;
+cursor: pointer;
+
+:hover {
+  background: #E89636;
+  color: #262A30;
+}
+`;
+
+export const About = styled.section`
+
+text-align: center;
+max-width: 1440px;
+margin-top: 20px;
+margin-left: auto;
+margin-right: auto;
+
+`;
+export const AboutTitle = styled.h2`
+display: block;
+font-weight: 700;
+position: relative;
+font-size: 56px;
+line-height: 66px;
+letter-spacing: 0.15em;
+color: #E89636;
+
+::before {
+  content: '';
+  display: block;
+  position: absolute;
+  // width: 530px;
+  flex-basis: calc((100% - 40px) / 2);
+  top:40px;
+  right: 0;
+  width: 450px;
+  margin-right: 40px;
+  border-bottom: 3px solid #E89636;
+}
+
+::after {
+  content: '';
+  display: block;
+  position: absolute;
+  // width: 530px;
+  top:40px;
+  left: 0;
+  width: 450px;
+  margin-left: 40px;
+  border-bottom: 3px solid #E89636;
+}
+`;
+
+export const LineStyle = styled.div`
+max-width: 1440px;
+margin-bottom: 40px;`;
+
+export const AboutText = styled.p`
+font-weight: 400;
+font-size: 24px;
+line-height: 36px;
+letter-spacing: 0.03em;
+color: #E89636;
+
+margin: 80px 20px;`;
+
+export const CardSet = styled.ul`
+margin-left: 80px;
+margin-right: 80px;
+margin-bottom: 40px;
+display: flex;
+  flex-wrap: wrap;
+  column-gap: 80px;
+  row-gap: 40px;`;
+
+export const Card = styled.li`
+flex-basis: calc((100% - 80px) / 2);
+display: block;
+width: 600px;
+height: 350px;
+background: transparent;
+`;
+
+export const CardTitle = styled.h3`
+display: block;
+width: 100%;
+background-color: #E89636;
+border-radius: 16px 16px 0px 0px;
+padding: 31.5px 0;
+font-weight: 700;
+font-size: 40px;
+line-height: 47px;
+letter-spacing: 0.03em;
+color: #262A30;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);`;
+
+export const CardBox = styled.div`
+padding:10px;
+// width: 594px;
+height: 220px;
+border-radius: 0px 0px 16px 16px;
+border-left: 3px solid #E89636;
+border-right: 3px solid #E89636;
+border-bottom: 3px solid #E89636;
+`;
+
+export const CardText = styled.p`
+font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 36px;
+text-align: center;
+letter-spacing: 0.03em;
+color: #E4D8C9;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+&+& {
+  margin-top: 30px;
+}`;
+
+
+// .carousel {
+//   position: relative;
+// }
+
+// .carousel-item {
+//   position: absolute;
+//   visibility: hidden;
+//   background: #fec901;
+//   width: 400px;
+//   height: 300px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   font-size: 26px;
+//   text-transform: uppercase;
+//   font-weight: bold;
+//   user-select: none;
+// }
+
+// .carousel-item.visible {
+//   visibility: visible;
+// }
